14c8bd73fee02ef2f9bdde028c8c3e4ee65bf9ef
Vladimir Kuznetsov <vavaka@gmail.com>
2011-09-17 07:06:34 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GithubListener::Application.config.secret_token = 'af0dd66ba1ffe8ad21986951b67c5fdc958fb91692be7f26ed59e1634c51ce69e1bb70017c15cd7573e61bd34c334681d0ff8d0cbceca7750e121a84074e0c38'
